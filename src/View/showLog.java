1bc301f9627be57d0ffef2e6bb77ef288fac44b0
qwang12 <qwang12@wpi.edu>
2016-12-14 16:18:40 -0500

    haha

--- src/View/showLog.java
+++ src/View/showLog.java
@@ -22,7 +22,7 @@ public class showLog extends JFrame {
 	 * Create the frame.
 	 */
 	public showLog() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 604, 503);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
